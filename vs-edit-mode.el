706fbe7c7ea3e5b0eaa74bbb89eecaea8242fa62
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-11 22:11:07 +0800

    Add src

---
+++ vs-edit-mode.el
@@ -0,0 +1,38 @@
+;;; vs-edit-mode.el ---   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-03-11 22:10:58
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description:
+;; Keyword:
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/vs-edit-mode
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;;
+;;
+
+;;; Code:
+
+
+
+(provide 'vs-edit-mode)
+;;; vs-edit-mode.el ends here
